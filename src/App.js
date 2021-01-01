616ccf9fe3f4500c34a7cbbd3e5985c7a77911a4
Deepanshu <67218112+deeta-max@users.noreply.github.com>
2021-01-01 21:26:11 +0530

    add css and deployed it on firbase left auth

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import { Button, FormControl, Input, InputLabel } from '@material-ui/core';
 import './App.css';
 import Todo from './Todo';
+import db from "./firebase";
+import firebase from "firebase";
 
 function App() {
 
   const [todos, setTodos] = useState([]);
   const [input, setInput] = useState("");
+
+  useEffect(() => {
+    db.collection("todos").orderBy("timestamp", "desc").onSnapshot(snapshot => {
+      setTodos(snapshot.docs.map(doc => ({id:doc.id, todo:doc.data().todo})))
+    })
+  }, []);
  
   function addTodo(event){
     event.preventDefault();
-    setTodos([...todos, input]);
+    db.collection("todos").add({
+      todo:input,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp()
+    });
     setInput("");
   };
 
   return (
     <div className="App">
-    <h1>Hello Deeta</h1> 
-    <form> 
+    <h1>Hello User</h1> 
+    <form className="add-form"> 
     <FormControl>
       <InputLabel htmlFor="my-input">Add new todo item</InputLabel>
-      <Input onChange={event => setInput(event.target.value)} value={input} placeholder="New Item"  id="my-input" aria-describedby="my-helper-text" />
+      <Input className="add_input" onChange={event => setInput(event.target.value)} value={input} placeholder="New Item"  id="my-input" aria-describedby="my-helper-text" />
     </FormControl>
-    <Button disabled={!input}  type="submit" onClick={addTodo} variant="contained" color="primary">
+    <Button className="add_button" disabled={!input}  type="submit" onClick={addTodo} variant="contained" color="primary">
     Add Todo
     </Button>
     </form>
--- src/Todo.js
+++ src/Todo.js
@@ -1,16 +1,65 @@
-import {  List, ListItem, ListItemAvatar, ListItemText } from '@material-ui/core';
-import React from 'react'
+import { Button,FormControl,Input,InputLabel,List, ListItem, ListItemText, makeStyles, Modal } from '@material-ui/core';
+import React, { useState } from 'react';
+import db from './firebase';
+import  './Todo.css';
+import DeleteIcon from '@material-ui/icons/Delete';
+
+const useStyles = makeStyles((themes) => ({
+    modal: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop:"-200px"        
+      },
+    paper:{
+        position:"absolute",
+        width:400,
+        backgroundColor:"#eeeeee",
+        border:"2px solid #000",
+        boxShadow:themes.shadows[5],
+        padding:themes.spacing(2,4,3),
+    },
+}))
 
 function Todo(props) {
+
+    const [close, setClose] = useState(false);
+    const classes = useStyles();
+    const [input, setInput] = useState("");
+
+    const handleOpen = e => {
+        db.collection("todos").doc(props.todo.id).set({
+            todo:input
+        }, {merge: true})
+        setClose(false);
+        setInput("")
+    };
+
     return (
-        <List >
-            <ListItem>
-            <ListItemAvatar>
-            </ListItemAvatar>
-                <ListItemText primary={props.todo} secondary="Dummy Deadline ⏰" />
+        <>
+        <Modal
+        className={classes.modal}
+        open = {close}
+        onClose = {e => setClose(false)} >
+        <div className={classes.paper}>
+            <h1 className="edit-heading">Edit Todo Item </h1>
+            <FormControl className="edit-input">
+            <InputLabel htmlFor="my-input">Add new todo item</InputLabel>
+            <Input  onChange={event => setInput(event.target.value)} value={input} placeholder="Edit Item"  id="my-input" aria-describedby="my-helper-text" />
+            </FormControl>
+           
+            <Button className="edit-input" disabled={!input} onClick = {handleOpen} variant="contained" color="primary">Update Todo</Button>
+        </div>    
+        </Modal>
+        <List className="todo_list" >
+            <ListItem className="todo_item">
+                <ListItemText primary={props.todo.todo} secondary="Dummy Deadline ⏰" />
             </ListItem>
+            <Button className="eidt" onClick={e => setClose(true)} variant="contained" color="primary">Edit</Button>
+            <DeleteIcon className="delete" onClick={event => db.collection("todos").doc(props.todo.id).delete()} />
+            
         </List>
-        
+        </>
     )
 }
 
